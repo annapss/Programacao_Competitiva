0338ce4c97dc187609f3d42c9c22101700efc771
annapss <annpss07@gmail.com>
2025-03-05 15:18:27 -0300

    Comentários com o link dos problemas

--- Maratona de Programação/UVA/uva_10424.cpp
+++ Maratona de Programação/UVA/uva_10424.cpp
@@ -1,3 +1,4 @@
+//https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=1365
 #include <iostream>
 #include <string>
 #include <math.h>
--- Maratona de Programação/UVA/uva_13034.cpp
+++ Maratona de Programação/UVA/uva_13034.cpp
@@ -1,3 +1,4 @@
+//https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=4932
 #include <iostream>
 using namespace std;
 

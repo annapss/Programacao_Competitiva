6a73bc1cc59893f200702ede5a8e38116caacd81
annapss <annpss07@gmail.com>
2025-03-05 15:17:29 -0300

    Solução do 10424

---
+++ Maratona de Programação/UVA/uva_10424.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    string nomeA, nomeB;
+    int totalNomeA, totalNomeB;
+    float porcentagem;
+    while(getline(cin, nomeA))
+    {
+        getline(cin, nomeB);
+        totalNomeA = 0;
+        totalNomeB = 0;
+        for(int i = 0; i < nomeA.length(); i++)
+        {
+            if((nomeA[i] >= 'a' && nomeA[i] <= 'z'))
+            {
+                totalNomeA += nomeA[i] - ('a' - 1);
+            }
+            else if(nomeA[i] >= 'A' && nomeA[i] <= 'Z')
+            {
+                totalNomeA += nomeA[i] - ('A' - 1);
+            }
+        }
+        int temp, resto;
+        while(totalNomeA >= 10)
+        {
+            temp = totalNomeA, resto;
+            totalNomeA = 0;
+            while(temp >= 10)
+            {
+                resto = temp % 10;
+                temp = temp / 10;
+                totalNomeA += resto;
+            }
+            totalNomeA += temp;
+        }
+        for(int i = 0; i < nomeB.length(); i++)
+        {
+            if((nomeB[i] >= 'a' && nomeB[i] <= 'z'))
+            {
+                totalNomeB += nomeB[i] - ('a' - 1);
+            }
+            else if(nomeB[i] >= 'A' && nomeB[i] <= 'Z')
+            {
+                totalNomeB += nomeB[i] - ('A' - 1);
+            }
+        }
+        while(totalNomeB >= 10)
+        {
+            temp = totalNomeB, resto;
+            totalNomeB = 0;
+            while(temp >= 10)
+            {
+                resto = temp % 10;
+                temp = temp / 10;
+                totalNomeB += resto;
+            }
+            totalNomeB += temp;
+        }
+        if(totalNomeA < totalNomeB)
+        {
+            porcentagem = ((float)totalNomeA * 100) / (float)totalNomeB;
+        }
+        else
+            porcentagem = ((float)totalNomeB * 100) / (float)totalNomeA;
+        cout << fixed;
+        cout.precision(2);
+        cout << porcentagem << " %\n";
+    }
+}
